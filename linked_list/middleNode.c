ef60be4941bec51cc63138f3997920dc8f960061
nefelibatala1993 <leuela1993@gmail.com>
2023-01-19 09:16:40 +0300

    add middleNode.c

---
+++ linked_list/middleNode.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * middleNode - finds the middle node of a ListNode linked list
+ * @head: address of the head node
+ *
+ * Return: On success, it returns the address of the head node
+*/
+struct ListNode* middleNode(struct ListNode* head)
+{
+    
+}
+
