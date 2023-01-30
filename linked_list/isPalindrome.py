d5a92554ac4132d0796331324bb9a3d48f9e48f7
nefelibatala1993 <leuela1993@gmail.com>
2023-01-30 22:39:16 +0300

    add file

---
+++ linked_list/isPalindrome.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        aux = head
+        stack = []
+
+        while aux is not None:
+            stack.append(aux.val)
+            aux = aux.next
+
+        while head is not None:
+            if head.val != stack.pop():
+                return False
+            head = head.next
+        return True
