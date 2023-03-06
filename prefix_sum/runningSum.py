6f38de080339acf626623befc598d022a2f774a3
nefelibatala1993 <leuela1993@gmail.com>
2023-03-06 17:11:38 +0300

    add runningSum.py

---
+++ prefix_sum/runningSum.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""LeetCode Problem #1480 -> Running Sum of 1d Array"""
+
+
+class Solution:
+    def runningSum(self, nums: list[int]) -> int:
+        prefixSum = []
+
+        prefixSum.append(nums[0])
+        curr_sum = nums[0]
+        for num in nums[1:]:
+            curr_sum += num
+            prefixSum.append(curr_sum)
+
+        return prefixSum
