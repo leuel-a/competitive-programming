c88ce0da0a24614f2d45df98063e5374a008ef7b
nefelibatala1993 <leuela1993@gmail.com>
2023-03-14 08:44:39 +0300

    added findAnagrams.py

--- sliding_window/findAnagrams.py
+++ sliding_window/findAnagrams.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-"""LeetCode Problem #438 --> Find all Anagrams in a String"""
-
+"""LeetCode #438 --> Find All Anagrams in a String"""
 from collections import Counter
 
 
 class Solution:
     def findAnagrams(self, s: str, p: str) -> list[int]:
-        i, j = 0, 0
-        total, p_dict = len(p), Counter(p)
+        i = j = 0
+        dict_s = {}
+        p_dict = Counter(p)
+        anagrams_index = []
 
+        while j < len(s):
+            if j - i + 1 > len(p):
+                i += 1
 
-
-
-sol = Solution()
-print(sol.findAnagrams("cbaebabacd", "abc"))
+            if j - i + 1 == len(p):
+                if Counter(s[i:j+1]) == p_dict:
+                    anagrams_index.append(i)
+            j += 1
+        return anagrams_index
