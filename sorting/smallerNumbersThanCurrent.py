809412b27101b35b41fd8e6cf6d7aa686bfb7b73
nefelibatala1993 <leuela1993@gmail.com>
2023-03-22 01:10:58 +0300

    added python3 solution

---
+++ sorting/smallerNumbersThanCurrent.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""LeetCode Problem #1365 --> How Many Numbers Are Smaller Than
+Current Number"""
+
+
+
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        smallerArr = []
+
+        for i in nums:
+            count = 0
+            for j in nums:
+                if j == i:
+                    continue
+                if i > j:
+                    count += 1
+            smallerArr.append(count)
+        return smallerArr
