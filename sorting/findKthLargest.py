d3c2f03cbfd427f098fb26db34df5acc97ed98a6
nefelibatala1993 <leuela1993@gmail.com>
2023-03-27 17:53:04 +0300

    added findKthLargest.py

---
+++ sorting/findKthLargest.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""LeetCode Problem #215 --> Kth Largest Element in an Array"""
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+
+        def findKthLargestHelper(start, end) -> int:
+            pivot, write = nums[start], start + 1
+            for read in range(start + 1, len(nums)):
+                if nums[read] < pivot:
+                    nums[read], nums[write] = nums[write], nums[read]
+                    write += 1
+
+            nums[start], nums[write - 1] = nums[write - 1], nums[start]
+
+            if k == (len(nums) - (write - 1)):
+                return nums[write - 1]
+            elif k < (len(nums) - (write - 1)):
+                return findKthLargestHelper(write, end)
+            return findKthLargestHelper(start, write - 2)
+        return findKthLargestHelper(0, len(nums) - 1)
