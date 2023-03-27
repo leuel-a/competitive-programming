dfddd79e6452368d764f6aaa8548fc576911d0a3
nefelibatala1993 <leuela1993@gmail.com>
2023-03-27 14:07:14 +0300

    added removeDuplicates.py

---
+++ two_pointers/removeDuplicates.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""LeetCode Problem #26 --> Remove Duplicates from Sorted Array"""
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i, j = 0, 1
+
+        while j < len(nums):
+            if nums[i] != nums[j]:
+                i += 1
+                nums[i] = nums[j]
+            j += 1
+        return i + 1
