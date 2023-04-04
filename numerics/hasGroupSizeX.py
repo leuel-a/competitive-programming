0a17077e1a93f9bd3e6ff8132c0e53b9df6b9193
nefelibatala1993 <leuela1993@gmail.com>
2023-04-04 16:40:44 +0300

    added hasGroupSizeX.py

---
+++ numerics/hasGroupSizeX.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""LeetCode Problem #914 --> X of a Kind in a Deck of Cards"""
+from collections import Counter
+
+
+class Solution:
+    def hasGroupsSizeX(self, deck: list[int]) -> bool:
+        frequency = Counter(deck)
+        _min = min(frequency.values())
+
+        if _min == 1:
+            return False
+        while _min > 1:
+            for val in frequency.values():
+                if val % _min != 0:
+                    _min -= 1
+                    break
+            else:
+                return True
+        return False
