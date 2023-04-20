588a46826c3afaa88855aeddbeeb6ee692715cc5
nefelibatala1993 <leuela1993@gmail.com>
2023-04-20 14:48:19 +0300

    added twoSum.py

---
+++ two_pointers/twoSum.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""LeetCode Problem #167 --> Two Sum 2 - Input Array Sorted"""
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i, j = 0, len(numbers) - 1
+
+        while i <= j:
+            if numbers[i] + numbers[j] == target:
+                return [i + 1, j + 1]
+
+            if numbers[i] + numbers[j] > target:
+                j -= 1
+            else:
+                i += 1
