3d8e543b1f602d7c284b0f15b2130c8b1a9c7bbe
nefelibatala1993 <leuela1993@gmail.com>
2023-07-21 19:10:17 +0300

    Added Is Palindrom.py

--- linked_list/isPalindrome.py
+++ linked_list/isPalindrome.py
@@ -1,8 +1,12 @@
+#!/usr/bin/python3
+"""LeetCode Problem #234 --> Palindrom Linked List"""
+from typing import Optional
+
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         aux = head
