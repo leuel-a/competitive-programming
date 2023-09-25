3fc0a23b1d379606184d794b8eb32829fa002e69
nefelibatala1993 <leuela1993@gmail.com>
2023-09-25 17:08:33 +0300

    pass pycodestyle

--- dynamic_programming/calculateMinimumHP.py
+++ dynamic_programming/calculateMinimumHP.py
@@ -6,23 +6,23 @@ from functools import cache
 class Solution:
     def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
 
-        def inbound(r: int, c:int) -> bool:
+        def inbound(r: int, c: int) -> bool:
             return 0 <= r < len(dungeon) and 0 <= c < len(dungeon[0])
-        
+
         @cache
         def dp(row: int, col: int) -> int:
             if row == len(dungeon) - 1 and col == len(dungeon[0]) - 1:
                 return -dungeon[row][col]
-            
+
             curr = float('inf')
             if inbound(row + 1, col):
                 curr = min(curr, dp(row + 1, col))
-            
+
             if inbound(row, col + 1):
                 curr = min(curr, dp(row, col + 1))
-            
+
             if dungeon[row][col] < 0 and curr < 0:
                 return -dungeon[row][col]
-            
+
             return curr - dungeon[row][col]
         return max(1, dp(0, 0) + 1)
