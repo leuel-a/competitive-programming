fd72bfed9ce7736c02e45fa793bddfe419635532
nefelibatala1993 <leuela1993@gmail.com>
2023-10-11 18:00:39 +0300

    add the solutions for the LCA problem in leetcode

--- trees/lowestCommonAncestor.py
+++ trees/lowestCommonAncestor.py
@@ -1,20 +1,26 @@
-#!/usr/bin/python3
-"""LeetCode #235 --> Lowest Common Ancestor"""
+"""Leetcode Problem #236 --> Lowest Common Ancestor"""
+
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if p.val <= root.val <= q.val:
-            return root
-        if q.val <= root.val <= p.val:
+        if root is None:
+            return None
+
+        if root == p or root == q:
             return root
 
-        if p.val > root.val and q.val > root.val:
-            return self.lowestCommonAncestor(root.right, p, q)
-        return self.lowestCommonAncestor(root.left, p, q)
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left is not None and right is not None:
+            return root
+
+        return left if left is not None else right
