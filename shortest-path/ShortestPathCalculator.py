7a815ce266cf192dc6e38ed599a6f0ee7f75679c
nefelibatala1993 <leuela1993@gmail.com>
2023-11-11 09:49:44 +0300

    add the Dijkstra Solution to the Design Graph With Shortest Path Calculator Problem

---
+++ shortest-path/ShortestPathCalculator.py
@@ -0,0 +1,41 @@
+"""Leetcode Problem #2642 --> Design Graph With Shortest Path Calculator"""
+from collections import defaultdict
+from typing import List
+
+
+class Graph:
+
+    def __init__(self, n: int, edges: List[List[int]]):
+        self.size = n
+        self.graph = defaultdict(list)
+
+        # Building the graph
+        for u, v, w in edges:
+            self.graph[u].append((v, w))
+
+    def addEdge(self, edge: List[int]) -> None:
+        u, v, w = edge
+        self.graph[u].append((v, w))
+
+    def shortestPath(self, node1: int, node2: int) -> int:
+        distances = [float('inf') for _ in range(self.size)]
+        distances[node1] = 0
+
+        heap = [(0, node1)]
+        while heap:
+            dist, node = heappop(heap)
+
+            if distances[node] < dist:
+                continue
+
+            for neighbour, weight in self.graph[node]:
+                distance = dist + weight
+                if distances[neighbour] > distance:
+                    distances[neighbour] = distance
+                    heappush(heap, (distance, neighbour))
+        return -1 if distances[node2] == float('inf') else distances[node2]
+
+# Your Graph object will be instantiated and called as such:
+# obj = Graph(n, edges)
+# obj.addEdge(edge)
+# param_2 = obj.shortestPath(node1,node2)
